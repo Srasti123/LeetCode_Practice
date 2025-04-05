3c07cf9cb558a4547e45b3d54481b5d7ccbf6271
Srasti123 <166982503+Srasti123@users.noreply.github.com>
2025-04-06 04:48:30 +0530

    Time: 0 ms (100%), Space: 10.7 MB (38.96%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        dp[i] = dp[i / 2] + (i % 2); // Right shift + last bit check
+    }
+
+    return dp;
+}
+};
